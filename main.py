f823ecd06f8da08b1c826a8c74508229e1b69579
Nathan Touchton <ntouchton@protonmail.com>
2023-05-09 21:48:41 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 """This is a bot program for cookie clicker that I'm making to learn how to use Selenium."""
 
-from bs4 import BeautifulSoup
+# from pprint import pprint
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.common.by import By
 from webdriver_manager.firefox import GeckoDriverManager
 
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
@@ -10,7 +11,17 @@ driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()
 driver.get("https://www.python.org/")
 # Extract date and name of upcoming events and print them to a nested dict
 
-thing = driver.find_element(by="class name", value="mw-page-title-main")
-print(thing.text)
+element = driver.find_element(By.CLASS_NAME, "event-widget")
+times = element.find_elements(By.TAG_NAME, "time")
+event_names = element.find_elements(By.TAG_NAME, "a")
+
+for e in times:
+    print(e.text)
+
+for e in event_names:
+    print(e.text)
 
 driver.quit()
+
+###CLass: medium-widget event-widget last
+# I need to find the timedate value and text from the time tags.###

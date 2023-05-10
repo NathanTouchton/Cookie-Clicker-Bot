42094dbe022fbd670c4ca0a4f432695885ef8fb8
Nathan Touchton <ntouchton@protonmail.com>
2023-05-10 14:05:00 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 """This is a bot program for cookie clicker that I'm making to learn how to use Selenium."""
 
-# from pprint import pprint
+from pprint import pprint
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.common.by import By
-# from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.firefox import GeckoDriverManager
 
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
@@ -18,15 +17,14 @@ event_names = element.find_elements(By.CSS_SELECTOR, "a:not([title='More Events'
 
 time_text = [t.text for t in times]
 event_names_text = [e.text for e in event_names]
-# print(time_text + event_names_text)
 
+event_dict = {
+    time_text.index(i): {
+        "time": i,
+        "event name": event_names_text[time_text.index(i)],
+    } for i in time_text
+}
 
+pprint(event_dict)
 
 driver.quit()
-filler_dict: {
-    0: {
-        "time": "00:00",
-        "name": "insert name here"
-
-    }
-}
